3c87329c9ce41cc677c4d194066bdcbc88719f2a
JahsonGA <jgonzalezallie@gmail.com>
2025-08-08 21:23:02 -0400

    Data parsing, getting data from api that would be useful for ML and sockets.

--- backend/utils/parseShipData.js
+++ backend/utils/parseShipData.js
@@ -15,7 +15,28 @@ async function ParseShippoData(carriersAndTracking) {
   };
 
   carriersAndTracking.forEach(entry => {
-    
+    /**
+     * tracking_number
+     * carrier
+     * servicelevel.token (optional but useful)
+     * address_from (city/state/zip/country)
+     * address_to (city/state/zip/country)
+     * eta
+     * original_eta
+     * test (boolean)
+     * tracking_status (entire object)
+     * tracking_history (array)
+     */
+    let track = entry.tracking.number;
+    let carrier = entry.carrier;
+    let token = entry.servicelevel.token;
+    let address_from = entry.tracking_history.location;
+    let address_to = entry.address_from;
+    let eta = entry.eta;
+    let original_eta = entry.original_eta;
+    let testing = entry.test;
+    let status = entry.tracking_history.status;
+    let history = entry.tracking_history;
   });
 
   return grouped;
